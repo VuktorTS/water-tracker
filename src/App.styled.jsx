9111f5dd612807b63be68dcd9f13e2487cc597f1
VuktorTS <skalina222@gmail.com>
2024-04-11 23:32:42 +0300

    add redux, services and page

--- src/App.styled.jsx
+++ src/App.styled.jsx
@@ -1,12 +1,4 @@
 import styled from 'styled-components';
-import example from './assets/background.png';
 import 'normalize.css';
 
-export const AppWrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-image: url(${example});
-  background-repeat: no-repeat;
-  background-position: left top;
-  background-size: 100% 100%;
-`;
+export const AppWrapper = styled.div``;
