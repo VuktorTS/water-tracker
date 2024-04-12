bc0818a50836d0d0289e916c3d942db69c1ebf67
VuktorTS <skalina222@gmail.com>
2024-04-13 00:34:27 +0300

    add persist provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import {App} from './App.jsx'; 
 import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+import { Provider } from 'react-redux';
+import { persistor } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/water-tracker">
-      <App />
-    </BrowserRouter>
+    <Provider store={persistor}>
+      <PersistGate>
+        <BrowserRouter basename="/water-tracker">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 );
