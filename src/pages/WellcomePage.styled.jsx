19978c035f4ac47248f6ea55023ea49174adac05
YuliiaTsaruk <130906916+YuliiaTsaruk@users.noreply.github.com>
2024-04-14 14:18:49 +0200

    fixed height Container WellcomePage content

--- src/pages/WellcomePage.styled.jsx
+++ src/pages/WellcomePage.styled.jsx
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  margin-bottom: 40px;
+
+  @media (min-width: 768px) {
+    margin-bottom: 50px;
+  }
+
   @media (min-width: 1440px) {
     display: flex;
     flex-direction: row;
