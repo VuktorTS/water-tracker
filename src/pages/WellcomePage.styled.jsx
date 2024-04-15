a74f633d4831ad5df0a2377d5e86524e43556377
YuliiaTsaruk <130906916+YuliiaTsaruk@users.noreply.github.com>
2024-04-15 21:28:36 +0200

    added margin-top to welcome page container

--- src/pages/WellcomePage.styled.jsx
+++ src/pages/WellcomePage.styled.jsx
@@ -4,9 +4,11 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 40px;
+  margin-top: 24px;
 
   @media (min-width: 768px) {
     gap: 60px;
+    margin-top: 40px;
   }
 
   @media (min-width: 1440px) {
@@ -14,5 +16,6 @@ export const Container = styled.div`
     justify-content: center;
     gap: 81px;
     align-items: flex-end;
+    margin-top: 80px;
   }
 `;
