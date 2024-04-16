8298a2f8057516d64bdaac133de1b1e63046a108
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-04-16 14:04:00 +0300

    0.1.0-16.04.24

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,27 +1,95 @@
-// import axios from 'axios';
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-// axios.defaults.baseURL = '';
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+axios.defaults.baseURL = 'https://project-node-wt-team4.onrender.com/api';
 
-// const registration = createAsyncThunk(
-//   'auth/register'
-// );
+const setAuthHeader = token => {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
 
-// const logIn = createAsyncThunk(
-//   'auth/login'
-// );
+const clearAuthHeader = () => {
+    axios.defaults.headers.common.Authorization = '';
+};
 
-// const logOut = createAsyncThunk(
-//   'auth/logout'
-// );
+export const registration = createAsyncThunk(
+  'auth/register', 
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post('/users/signup', credentials);
+      return res.data
+    } catch (e) {
+        if (e.response) {
+            return thunkAPI.rejectWithValue(e.response.data.message);
+        } else {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+  }
+);
 
-// const fetchCurrentUser = createAsyncThunk(
-//   'auth/refresh'
-// );
+export const logIn = createAsyncThunk(
+  'auth/login',
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post('/users/signin', credentials);
+      setAuthHeader(res.data.token);
+      return res.data
+    } catch (e) {
+        if (e.response) {
+            return thunkAPI.rejectWithValue(e.response.data.message);
+        } else {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+  }
+);
 
-// const operations = {
-//   registration,
-//   logOut,
-//   logIn,
-//   fetchCurrentUser,
-// };
-// export default operations;
+export const logOut = createAsyncThunk(
+  'auth/logout',
+  async(_, thunkAPI) => {
+    try {
+      const res = await axios.post('/users/signout');
+      if (res) {
+        clearAuthHeader();
+        return res.data
+      }
+  } catch (e) {
+        if (e.response) {
+            return thunkAPI.rejectWithValue(e.response.data.message);
+        } else {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+  }
+);
+
+export const getCurrUserParams = createAsyncThunk(
+  'auth/params',
+  async (_, thunkAPI) => {
+    try {
+      const res = await axios.get('users/current');
+      return res.data;
+    } catch (e) {
+      if (e.response) {
+        return thunkAPI.rejectWithValue(e.response.data.message);
+      } else {
+        return thunkAPI.rejectWithValue(e.message);
+      }
+    }
+  }
+);
+
+export const setCurrentUser = createAsyncThunk(
+  'auth/refresh',
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.patch('users/', credentials);
+      return res.data
+    } catch (e) {
+        if (e.response) {
+            return thunkAPI.rejectWithValue(e.response.data.message);
+        } else {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+  }
+);
--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,25 +1,86 @@
 import { createSlice } from '@reduxjs/toolkit';
-// import{registration} from "./authOperations"
+import{registration, logIn, logOut, getCurrUserParams, setCurrentUser} from "./authOperations"
 
 const initialState = {
   user: { name: null, email: null },
+  specialMess: '',
   token: null,
   isLoggedIn: false,
-  isRefreshing: false,
-  isAuthLoading: false,
+  isLoading: false,
+  error: null
+};
+
+const forPending = (state) => {
+    state.isLoading = true;
+    state.error = null;
+};
+
+const forRejected = (state, action) => {
+    state.isLoading = false;
+    state.error = action.payload;
+    if(state.error === '401'){
+        state.token = null;
+      state.isLoggedIn = false;
+      state.user = { name: null, email: null };
+      state.specialMess = '';
+    }
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  // extraReducers:
-  //   builder => {
-  //   builder.addCase(registration.fulfilled, (state, { payload }) => {
-  //     state.user = payload.user;
-  //     state.token = payload.token;
-  //     state.isLoggedIn = true;
-  //     state.isAuthLoading = false;
-  //   });
-  // },
+  extraReducers: builder => {
+    builder
+      .addCase(registration.pending, forPending)
+      .addCase(registration.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        // state.token = payload.token;
+        state.isLoggedIn = true;
+        state.isLoading = false;
+        state.error = null;
+        state.specialMess = '';
+      })
+      .addCase(registration.rejected, forRejected)
+      .addCase(logIn.pending, forPending)
+      .addCase(logIn.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+        state.isLoading = false;
+        state.error = null;
+        state.specialMess = '';
+      })
+      .addCase(logIn.rejected, forRejected)
+      .addCase(logOut.pending, forPending)
+      .addCase(logOut.fulfilled, (state, { payload }) => {
+        state.user = { name: null, email: null };
+        state.specialMess = payload.message;
+        state.token = null;
+        state.isLoggedIn = false;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(logOut.rejected, forRejected)
+      .addCase(getCurrUserParams.pending, forPending)
+      .addCase(getCurrUserParams.fulfilled, (state, { payload }) => {
+        state.user = payload;
+        state.specialMess = '';
+        state.token = null;
+        state.isLoggedIn = false;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(getCurrUserParams.rejected)
+    .addCase(setCurrentUser.pending, forPending)
+      .addCase(setCurrentUser.fulfilled, (state, { payload }) => {
+        state.user = payload;
+        state.specialMess = '';
+        state.token = null;
+        state.isLoggedIn = false;
+        state.isLoading = false;
+        state.error = null;
+      })
+    .addCase(setCurrentUser.rejected)
+  },
 });
 export default authSlice.reducer;
