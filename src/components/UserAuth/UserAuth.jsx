d3e666440c0032d7d136d45fc1ffc96babc31a8f
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-04-17 19:44:30 +0300

    0.0.0-17.04.24

--- src/components/UserAuth/UserAuth.jsx
+++ src/components/UserAuth/UserAuth.jsx
@@ -1,13 +1,6 @@
-import css from './UserAuth.module.css';
-import { Link } from "react-router-dom"
-
 export const UserAuth = () => {
   
   return (
-    <div className={css.allDivLink}>
-      <Link to="/signin" style={{minHeight: '100%'}}>
-      <button type="button" className={css.buttonLink}>Sign In <svg className={css.svgSpan}/></button>
-      </Link>
-    </div>
+    <>UserAuth</>
   )
 }
