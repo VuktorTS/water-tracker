70b26b35b27f1db30080101e8317ea9a3118a9ec
ovchrk <113932934+ovchrk@users.noreply.github.com>
2024-04-17 22:19:07 +0300

    fix: component name changed, added feature of modal content rendering depending on the type of modal

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,37 @@
-import ModalWrapper from '../components/ModalWrapper/ModalWrapper.jsx';
-import AddWaterModal from '../components/AddWaterModal/AddWaterModal.jsx';
+import ModalWrapper from '../components/ModalWrapper/ModalWrapper'; 
+import TodayListModal from '../components/TodayListModal/TodayListModal.jsx';
 import React, { useState } from 'react';
 
+const MODAL_TYPES = { ADD: "ADD", EDIT: "EDIT" };
+const data = {time: '07:15 AM', ml: 250, id: 9843576}
+
 const HomePage = () => {
-        const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [modalType, setModalType] = useState("");
+
         const onClose = () => {
         setModal(false);
   };
-        const onOpen = () => {
-        setModal(true);
+        const onOpen = (type) => {
+          setModal(true);
+          setModalType(type);
   };
 
-    return (
-        <div>Home page
-            <button onClick={onOpen}>Add water</button>
-      {modal && (
+  return (<>
+    <div>Home Page</div>
+      <button onClick={() => onOpen(MODAL_TYPES.ADD)}>Add water</button>
+      <button onClick={() => onOpen(MODAL_TYPES.EDIT)}>Edit</button>
+      {modal && modalType  === MODAL_TYPES.ADD && (
         <ModalWrapper onClose={onClose} title="Add water">
-            <AddWaterModal></AddWaterModal>
+            <TodayListModal title={'Choose a value:'} onClose={onClose}></TodayListModal>
         </ModalWrapper>
       )}
-        </div>
+      {modal && modalType  === MODAL_TYPES.EDIT && (
+        <ModalWrapper onClose={onClose} title="Edit the entered amount of water">
+            <TodayListModal title={'Correct entered data:'} onClose={onClose} data={data}></TodayListModal>
+        </ModalWrapper>
+      )}
+    </>
   );
 };
 
