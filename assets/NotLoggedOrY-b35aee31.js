8fd692e46de4daa94b1f68dfea964f49936bd8f0
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-04-24 16:02:54 +0000

    Deploying to gh-pages from @ VuktorTS/water-tracker@3cbf067154f5a23adb5870ed95f3aac52cfc8e1d 🚀

---
+++ assets/NotLoggedOrY-b35aee31.js
@@ -0,0 +1,2 @@
+import{u as e,i as t,j as g,N as a}from"./index-b416816b.js";const n=({component:o,redirectTo:s="/"})=>e(t)?o:g.jsx(a,{to:s});export{n as default};
+//# sourceMappingURL=NotLoggedOrY-b35aee31.js.map
