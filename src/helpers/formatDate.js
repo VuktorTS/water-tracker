1b2ddb7acc50cc6aad4006484e955b318b043b35
TaraskiN85 <taras12304@i.ua>
2024-04-24 07:33:08 +0300

    fix water percentage

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -4,3 +4,8 @@ export function formatTime(dateString, formatString = 'HH:mm') {
   const date = addHours(parseISO(dateString), -3);
   return format(date, formatString);
 }
+
+const currentDate = new Date();
+const currentDay = format(currentDate, 'dd');
+const currentMonth = format(currentDate, 'MMMM');
+export const stringDate = `${currentDay}, ${currentMonth}`;
