d88ed6232e8a15bc77a99a34cea25e43c98f91a6
Reiterovich <reiterovich.andriy@gmail.com>
2024-04-25 12:24:52 +0300

    змінив лого

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,17 +1,18 @@
-import { HomeLink } from "./Logo.styled"
-import simpleImage from '../../img/Logo.jpg';
+import { HomeLink } from './Logo.styled';
+import simpleImage from '../../img/Logo.svg';
 
 // ще потрібно буде доробити рендер зображення під ретину
 // import retinaImage from '../../img/Logo-2x.jpg';
-// import { useSelector } from "react-redux";
+// import { useSelector } from 'react-redux';
 
 export const Logo = () => {
-  const isLoggedIn = true //useSelector(selectIsLoggedIn);
+  const isLoggedIn = true; //useSelector(selectIsLoggedIn);
   const path = isLoggedIn ? '/home' : '/welcome';
   return (
     <HomeLink to={path}>
       {/* <ImageDiv simple={simpleImage} retina={retinaImage}></ImageDiv> */}
+
       <img src={simpleImage} alt="Water Tracker Logo" />
     </HomeLink>
-  )
-}
+  );
+};
--- src/components/Logo/Logo.styled.js
+++ src/components/Logo/Logo.styled.js
@@ -3,18 +3,18 @@ import styled from 'styled-components';
 
 export const HomeLink = styled(NavLink)``;
 
-// export const ImageDiv = styled.div`
-//   background-image: url(${(props) => props.simple});
-//   /* background-image: url('../../img/Logo.jpg'); */
-//   background-repeat: no-repeat;
-//   background-size: cover;
-//   height: 48px;
-//   width: 102px;
+export const ImageDiv = styled.div`
+  background-image: url(${(props) => props.simple});
+  /* background-image: url('../../img/Logo.jpg'); */
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 48px;
+  width: 102px;
 
-//   @media (min-device-pixel-ratio: 2),
-//     (min-resolution: 192dpi),
-//     (min-resolution: 2dppx) {
-//       background-image: url(${(props) => props.simple});
-//       /* background-image: url('../../img/Logo-2x.jpg'); */
-//   }
-// `;
+  @media (min-device-pixel-ratio: 2),
+    (min-resolution: 192dpi),
+    (min-resolution: 2dppx) {
+    background-image: url(${(props) => props.simple});
+    /* background-image: url('../../img/Logo-2x.jpg'); */
+  }
+`;
