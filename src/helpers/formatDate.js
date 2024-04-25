9a384dbd89503ec85780575e9178c22a2973b840
TaraskiN85 <taras12304@i.ua>
2024-04-25 17:46:04 +0300

    fix time options in TimeForm

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -1,4 +1,4 @@
-import { addHours, format, parseISO, addMinutes, startOfHour } from 'date-fns';
+import { addHours, format, parseISO, addMinutes, startOfDay } from 'date-fns';
 
 export function formatTime(dateString, formatString = 'p') {
   const date = addHours(parseISO(dateString), -3);
@@ -13,18 +13,17 @@ export const stringDate = `${currentDay}, ${currentMonth}`;
 
 export const getTimeOptions = () => {
   const options = [];
-  const baseTime = startOfHour(new Date());
+  const baseTime = startOfDay(new Date());
+  const currentTime = new Date(); // Current time
   const timeFormat = 'hh:mm a';
 
-  const currentMinute = baseTime.getMinutes();
-  const nearestMultipleOfFive = Math.ceil(currentMinute / 5) * 5;
-
-  for (let i = nearestMultipleOfFive; i < 60; i += 5) {
-    const currentTime = addMinutes(baseTime, i);
-    const time = format(currentTime, timeFormat);
+  for (let i = 0; i < 1440; i += 5) {
+    const currentIterationTime = addMinutes(baseTime, i);
+    if (currentIterationTime > currentTime) break;
+    const time = format(currentIterationTime, timeFormat);
     options.push({
       time,
-      id: format(currentTime, 'HH:mm'),
+      id: format(currentIterationTime, 'HH:mm'),
     });
   }
 
