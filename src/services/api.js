573d66fb90d04c01f646a3e40c1504d9cdd9359a
ViktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-04-25 11:14:40 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:3000/api';
+axios.defaults.baseURL = 'https://project-node-wt-team4.onrender.com/api';
 
 export const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
