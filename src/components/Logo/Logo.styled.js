570e22a79b8a79fe07498f6dd8d444d85399ebf1
YuliiaTsaruk <130906916+YuliiaTsaruk@users.noreply.github.com>
2024-04-25 12:22:07 +0200

    fixed logo

--- src/components/Logo/Logo.styled.js
+++ src/components/Logo/Logo.styled.js
@@ -2,19 +2,3 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const HomeLink = styled(NavLink)``;
-
-// export const ImageDiv = styled.div`
-//   background-image: url(${(props) => props.simple});
-//   /* background-image: url('../../img/Logo.jpg'); */
-//   background-repeat: no-repeat;
-//   background-size: cover;
-//   height: 48px;
-//   width: 102px;
-
-//   @media (min-device-pixel-ratio: 2),
-//     (min-resolution: 192dpi),
-//     (min-resolution: 2dppx) {
-//       background-image: url(${(props) => props.simple});
-//       /* background-image: url('../../img/Logo-2x.jpg'); */
-//   }
-// `;
